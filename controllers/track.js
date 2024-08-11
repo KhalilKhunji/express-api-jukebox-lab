d53b6df8c4d161569cb12a5106b092843a562728
KhalilKhunji <khalilkhunji@gmail.com>
2024-08-11 14:33:25 +0300

    index route implemented

--- controllers/track.js
+++ controllers/track.js
@@ -8,7 +8,16 @@ router.post('/', async (req, res) => {
       res.status(201).json(newTrack);
     } catch (error) {
       res.status(500).json({ error: 'oops something went wrong' });
-    }
+    };
+});
+
+router.get('/', async (req, res) => {
+    try {
+      const allTracks = await Track.find();
+      res.status(200).json(allTracks);
+    } catch (error) {
+      res.status(500).json({ error: 'oops something went wrong' });
+    };
 });
 
 module.exports = router;
